af5133fd101a15e576b4a0ca38f401f839a15b21
Eric Kansa <kansaeric@gmail.com>
2022-02-17 17:20:06 -0800

    Update gunicorn invocation

--- start/start_oc.sh
+++ start/start_oc.sh
@@ -10,4 +10,5 @@ echo "Start the Open Context server";
 # python /open-context-py/manage.py runserver 0.0.0.0:8000 &
 
 echo "Start the Gunicorn to WSGI";
-python gunicorn /open-context-py/opencontext.wsgi &
+# python gunicorn --bind=0.0.0.0:8000 opencontext_py.wsgi:application &
+# python gunicorn -w 2 -b 0.0.0.0:8000 --chdir /open-context-py opencontext_py.wsgi:application --reload --timeout 3600
