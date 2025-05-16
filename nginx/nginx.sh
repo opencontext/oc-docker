4c528acfd40396a65ef3db300dd5bccbab463247
Eric Kansa <kansaeric@gmail.com>
2025-05-15 20:33:03 -0700

    Change permissions function

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -32,7 +32,7 @@ update_static_permissions() {
 
 check_anubis_permissions() {
     while true; do
-        find /oc-anubis-img -type d -exec chmod 755 {} \;
+        find /oc-anubis-img -type d -exec chmod -R 755 {} \;
         find /oc-anubis-img -type f -exec chmod 644 {} \;
         sleep 3600  # Check every hour
     done
