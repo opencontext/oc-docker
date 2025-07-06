c4418446c45d9e4ef4a5e31a4da4a4394271085c
Eric Kansa <kansaeric@gmail.com>
2025-07-06 02:53:35 -0700

    fix syntax bug

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -53,7 +53,7 @@ for i in "${!domain_list[@]}"; do
   certbot certonly \
     --webroot \
     -w "/var/www/certbot/$domain" \
-    -d $domain -d \
+    -d $domain \
     $test_cert_arg \
     $email_arg \
     --rsa-key-size "${CERTBOT_RSA_KEY_SIZE:-4096}" \
