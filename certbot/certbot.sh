1cde9639d1b129064129642f3f1de3423f985a53
Eric Kansa <kansaeric@gmail.com>
2025-07-06 02:46:55 -0700

    No www

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -53,7 +53,7 @@ for i in "${!domain_list[@]}"; do
   certbot certonly \
     --webroot \
     -w "/var/www/certbot/$domain" \
-    -d $domain -d www.$domain \
+    -d $domain -d \
     $test_cert_arg \
     $email_arg \
     --rsa-key-size "${CERTBOT_RSA_KEY_SIZE:-4096}" \
